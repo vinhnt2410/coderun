4b6bfee5dbd32eea9b48b3ff80c51f80d28c9702
Vinh Nguyen Trong <vinhnt2410@gmail.com>
2022-05-29 16:09:38 +0700

    logger: add log variable for LOGVNT
    Signed-off-by: Vinh Nguyen Trong <vinhnt2410@gmail.com>

--- logger/include/LOGVNT.h
+++ logger/include/LOGVNT.h
@@ -2,18 +2,19 @@
 #define __LOGVNT_HEADER__
 #include <time.h>
 
-// #define LOGVNT_ENABLE
-#ifndef LOGVNT_ENABLE
+#define LOGVNT_ENABLE
+#ifdef LOGVNT_ENABLE
 
-#define LOGVNT_F __FILE__, __LINE__, __func__
-#define LOGVNT_T(tvnt) tvnt->tm_hour, tvnt->tm_min, tvnt->tm_sec
-#define LOGVNINFO(tvnt) LOGVNT_T(tvnt), LOGVNT_F
+#define LOGVNT_F           __FILE__, __LINE__, __func__
+#define LOGVNT_T(tvnt)     tvnt->tm_hour, tvnt->tm_min, tvnt->tm_sec
+#define LOGVNTINFO(tvnt)   LOGVNT_T(tvnt), LOGVNT_F
 static time_t svnt;
 static struct tm* cvnt;
-#define LOGVNT(msg) 			\
-svnt = time(NULL); 			\
-cvnt = localtime(&svnt); 	\
-printf("[%02d:%02d:%02d][%s:%d], %s - %s\n", LOGVNTINFO(cvnt), msg);
+
+#define LOGVNT(format, ...)         \
+   svnt = time(NULL);         \
+   cvnt = localtime(&svnt);   \
+   printf("[%02d:%02d:%02d][%s:%d], %s - " format "\n", LOGVNTINFO(cvnt), ##__VA_ARGS__);
 #else // DISABLE LOGVNT
 #define LOGVNT(...)
 #endif
